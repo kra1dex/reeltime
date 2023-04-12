6d7375e8a7a1faa2fc9c6c3204264ac432c1e50f
Grisha <grishamalanchuk@gmail.com>
2023-04-12 13:21:27 +0300

    serializer fixes

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,15 +1,38 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from movies.models import Movie, Director
 
 
-class DirectorSerializer(ModelSerializer):
+class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
         fields = '__all__'
 
 
-class MovieSerializer(ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
+    directors = DirectorSerializer(many=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+
+class MovieCreateSerializer(serializers.ModelSerializer):
+    directors = DirectorSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+    def is_valid(self, *, raise_exception=False):
+        super().is_valid()
+        self.directors = self.validated_data.pop('directors')
+
+    def create(self, validated_data):
+        movie = Movie.objects.create(**validated_data)
+
+        for director in self.directors:
+            director, _ = Director.objects.get_or_create(name=director['name'], surname=director['surname'])
+            movie.directors.add(director)
+
+        return movie
--- movies/views.py
+++ movies/views.py
@@ -2,12 +2,17 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.viewsets import ModelViewSet
 
 from movies.models import Movie, Director
-from movies.serializers import MovieSerializer, DirectorSerializer
+from movies.serializers import MovieSerializer, DirectorSerializer, MovieCreateSerializer
 
 
 class MovieListCreateAPIView(ListCreateAPIView):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return MovieCreateSerializer
+        else:
+            return MovieSerializer
 
 
 class MovieRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
