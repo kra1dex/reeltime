26eedd9f3e23f85a2d938136fcf09a7f30da5cca
Grisha <grishamalanchuk@gmail.com>
2023-04-14 23:04:44 +0300

    swagger

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ RUN poetry config virtualenvs.create false
 COPY poetry.lock pyproject.toml ./
 RUN poetry install -n --no-root --no-dev
 
+EXPOSE 8000
+
 COPY . ./
--- reeltime/settings.py
+++ reeltime/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_spectacular',
 
     'movies',
 ]
@@ -126,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'ReelTime API',
+    'VERSION': 'v1',
+}
