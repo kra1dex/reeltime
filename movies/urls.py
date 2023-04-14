a5d19f6fef8279c55640449324af6e8f13eff629
Grisha <grishamalanchuk@gmail.com>
2023-04-14 21:33:20 +0300

    get movies list test

--- movies/urls.py
+++ movies/urls.py
@@ -9,7 +9,6 @@ router.register('directors', views.DirectorViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 
-    # Movies
     path('movies/', views.MovieListCreateAPIView.as_view(), name='movie-list-create'),
     path('movies/<int:pk>/', views.MovieRetrieveUpdateDestroyAPIView.as_view(), name='movie-retrieve-update-destroy'),
 ]
