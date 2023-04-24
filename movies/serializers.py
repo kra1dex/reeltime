dab5dd44bc108b772eed3d47575a08c7c7cf7500
Grisha <grishamalanchuk@gmail.com>
2023-04-24 23:35:55 +0300

    update method for DirectorSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -17,6 +17,24 @@ class DirectorSerializer(serializers.ModelSerializer):
 
         return director
 
+    def update(self, instance, validated_data):
+        if self.context['request'].method == 'PATCH':
+            validated_data['name'] = validated_data['name'].lower() if 'name' in validated_data else instance.name
+            validated_data['surname'] = validated_data['surname'].lower() if 'surname' in validated_data else instance.surname
+
+        if self.context['request'].method == 'PUT':
+            validated_data['name'] = validated_data['name'].lower()
+            validated_data['surname'] = validated_data['surname'].lower()
+
+        try:
+            director = Director.objects.get(name=validated_data['name'], surname=validated_data['surname'])
+            if director.id != instance.id:
+                raise serializers.ValidationError({'director': ['Director already exists.']})
+        except Director.DoesNotExist:
+            director = super().update(instance, validated_data)
+
+        return director
+
 
 class MovieSerializer(serializers.ModelSerializer):
     directors = serializers.PrimaryKeyRelatedField(many=True, queryset=Director.objects.all())
