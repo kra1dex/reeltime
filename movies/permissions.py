8fc547b996ad034e7c4766ebae88c07a91fe54ba
Grisha <grishamalanchuk@gmail.com>
2023-04-24 23:53:14 +0300

    refactoring

--- movies/permissions.py
+++ movies/permissions.py
@@ -3,10 +3,9 @@ from rest_framework import permissions
 
 class IsAdminOrPublished(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_superuser or request.user.is_staff:
-            return True
-        else:
+        if not request.user.is_superuser or not request.user.is_staff:
             queryset = view.get_queryset()
             queryset = queryset.filter(status='publish')
             view.queryset = queryset
-            return True
+
+        return True
--- movies/serializers.py
+++ movies/serializers.py
@@ -9,20 +9,20 @@ class DirectorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
+        validated_data['name'] = validated_data['name'].lower()
+        validated_data['surname'] = validated_data['surname'].lower()
+
         try:
             Director.objects.get(name=validated_data['name'], surname=validated_data['surname'])
             raise serializers.ValidationError({'director': ['Director already exists.']})
         except Director.DoesNotExist:
-            director = Director.objects.create(name=validated_data['name'], surname=validated_data['surname'])
-
-        return director
+            return super().create(validated_data)
 
     def update(self, instance, validated_data):
         if self.context['request'].method == 'PATCH':
             validated_data['name'] = validated_data['name'].lower() if 'name' in validated_data else instance.name
             validated_data['surname'] = validated_data['surname'].lower() if 'surname' in validated_data else instance.surname
-
-        if self.context['request'].method == 'PUT':
+        elif self.context['request'].method == 'PUT':
             validated_data['name'] = validated_data['name'].lower()
             validated_data['surname'] = validated_data['surname'].lower()
 
@@ -31,9 +31,7 @@ class DirectorSerializer(serializers.ModelSerializer):
             if director.id != instance.id:
                 raise serializers.ValidationError({'director': ['Director already exists.']})
         except Director.DoesNotExist:
-            director = super().update(instance, validated_data)
-
-        return director
+            return super().update(instance, validated_data)
 
 
 class MovieSerializer(serializers.ModelSerializer):
