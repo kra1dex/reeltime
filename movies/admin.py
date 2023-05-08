a1b6784f981ae059f77dc12ce3625ba1172f57b6
Grisha <grishamalanchuk@gmail.com>
2023-05-08 22:38:32 +0300

    UserMovieRelation model

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from movies.models import Director, Movie
+from movies.models import Director, Movie, UserMovieRelation
 
 admin.site.register(Director)
 admin.site.register(Movie)
+admin.site.register(UserMovieRelation)
--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Director(models.Model):
     name = models.CharField(max_length=50)
@@ -20,6 +22,25 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     directors = models.ManyToManyField(Director)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='owner_movies')
+    spectators = models.ManyToManyField(User, through='UserMovieRelation', related_name='spectators_movies')
 
     def __str__(self):
         return f"ID: {self.id} | {self.title}"
+
+
+class UserMovieRelation(models.Model):
+    RATING = [
+        (1, 'Bad'),
+        (2, 'Normal'),
+        (3, 'Good'),
+        (4, 'Fine'),
+        (5, 'Incredible')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(choices=RATING, null=True)
+
+    def __str__(self):
+        return f"ID: {self.id} | {self.movie.title}. {self.user.username}"
