b349f3647640e77ea4e47489280fa490cc39b7ce
Grisha <grishamalanchuk@gmail.com>
2023-06-29 18:04:14 +0300

    debug toolbar for docker

--- reeltime/settings.py
+++ reeltime/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import socket
 from pathlib import Path
 from decouple import config
 
@@ -63,9 +64,9 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
+if DEBUG:
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
 ROOT_URLCONF = 'reeltime.urls'
 
